3709b28ee093bd6d29d7e7ca1f6299740e23e4f4
Bohan <bohan.huang@coolbe.com.tw>
2025-05-23 09:36:22 +0800

    save

--- vue2-web/src/ts/config.ts
+++ vue2-web/src/ts/config.ts
@@ -5,22 +5,22 @@ let LINE_LIFF_ID = "";
 
 switch (location.hostname) {
     // 正式機
-    case "ud-modules-vue2.udons.site":
-        BASE_URL = "https://mock.udons.site";
+    case "ud-modules-vue2.udons.space":
+        BASE_URL = "https://mock.udons.space";
         LINE_LOGIN_CHANNEL_ID = "1655285115";
         LINE_OA_URL = "https://line.me/R/ti/p/@524wuemo";
         LINE_LIFF_ID = "1655285115-w926gzYP";
         break;
     // 測試機
-    case "ud-modules-vue2.udons.site":
-        BASE_URL = "https://mock.udons.site";
+    case "ud-modules-vue2.udons.space":
+        BASE_URL = "https://mock.udons.space";
         LINE_LOGIN_CHANNEL_ID = "1655285115";
         LINE_OA_URL = "https://line.me/R/ti/p/@524wuemo";
         LINE_LIFF_ID = "1655285115-w926gzYP";
         break;
     // 本機開發
     default:
-        BASE_URL = "https://mock.udons.site";
+        BASE_URL = "https://mock.udons.space";
         LINE_LOGIN_CHANNEL_ID = "1655285115";
         LINE_OA_URL = "https://line.me/R/ti/p/@524wuemo";
         LINE_LIFF_ID = "1655285115-AYjbLKMr";
