680ef6192e0dcb9e51c8c1f82cb1c6c7da6e03be
Bohan <bohan.huang@coolbe.com.tw>
2025-06-04 09:31:45 +0800

    save

--- vue2-web/dist/js/config.js
+++ vue2-web/dist/js/config.js
@@ -5,14 +5,14 @@ let LINE_LIFF_ID = "";
 
 switch (location.hostname) {
     // 正式機
-    case "ud-modules-vue2.udons.space":
+    case "ud-modules-2.udons.space":
         BASE_URL = "https://mock.udons.space";
         LINE_LOGIN_CHANNEL_ID = "1655285115";
         LINE_OA_URL = "https://line.me/R/ti/p/@524wuemo";
         LINE_LIFF_ID = "1655285115-w926gzYP";
         break;
     // 測試機
-    case "ud-modules-vue2.udons.space":
+    case "ud-modules-2.udons.space":
         BASE_URL = "https://mock.udons.space";
         LINE_LOGIN_CHANNEL_ID = "1655285115";
         LINE_OA_URL = "https://line.me/R/ti/p/@524wuemo";
